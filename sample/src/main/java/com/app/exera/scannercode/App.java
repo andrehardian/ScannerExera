d833c6ff7c4200ec1c7eb78f3372fe68880a7807
Andre Hardian <andrehardian@gmail.com>
2018-03-23 11:59:41 +0700

    update

---
+++ sample/src/main/java/com/app/exera/scannercode/App.java
@@ -0,0 +1,10 @@
+package com.app.exera.scannercode;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by AndreHF on 3/22/2018.
+ */
+
+public class App extends MultiDexApplication {
+}
